40ab27a1d5dbe72eb23079de28c29525ec247e2e
sameerkitkat <sameerkitkat4.4@gmail.com>
2019-03-21 23:18:32 +0530

    simplifying helper method

--- Valid Parantheses/ValidParantheses.java
+++ Valid Parantheses/ValidParantheses.java
@@ -1,30 +1,39 @@
-import java.util.HashMap;
 import java.util.Stack;
 class ValidParantheses{
 
     private boolean validParantheses(String string){
-        HashMap<Character,Character> map = new HashMap<Character,Character>();
-        map.put(')', '(');
-        map.put(']', '[');
-        map.put('}', '{');
         Stack <Character> stack = new Stack <Character>();
         for (int i=0;i<string.length();i++){
             char c = string.charAt(i);
-            if (map.containsKey(c)){
-                char topElement = stack.empty() ? '#' : stack.pop();
-                if (topElement!=map.get(c)){
-                    return false;
-                }
+            if (c=='(' || c=='{' || c=='['){
+                stack.push(c);
+            }
+            else if (!stack.isEmpty() && isParantheses(stack.peek(),c)){
+                stack.pop();
             }
             else{
-                stack.push(c);
+                return false;
             }
         }
         return stack.isEmpty();
     }
 
+    private boolean isParantheses(char left,char right){
+         if (left=='(' && right==')'){
+             return true;
+         } else if (left=='{' && right=='}'){
+             return true;
+         }
+         else if (left=='[' && right==']'){
+             return true;
+         }
+         else{
+             return false;
+         }
+    }
+
     public static void main(String[] args){ 
-        String string = "[][]}";
+        String string = "([()])}";
         ValidParantheses validparantheses = new ValidParantheses();
         boolean isValid = validparantheses.validParantheses(string);
         System.out.println(isValid);
